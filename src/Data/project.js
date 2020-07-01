b02e8c0c6ee6b2dd098d6f4db45ea8996fd33d01
Raj <raj.geddadi@gmail.com>
2020-07-01 04:37:37 -0500

    first push

---
+++ src/Data/project.js
@@ -0,0 +1,29 @@
+import swds from '../Assets/Swds.png';
+import UWLogo from '../Assets/UWLogo.jpg';
+import web_dev from '../Assets/web_dev.jpg';
+
+const PROJECTS =[
+
+  {
+    id:1,
+    title: 'Enterprise Operation Specialist',
+    discription: '"Working with Javascript to Automate the production jobs using Workload Automation. Execution, scheduling and monitoring and providing support as needed for all the automated and add hoc job in production"',
+    image: swds
+  },
+
+  {
+    id:2,
+    title: 'Enterprise Datawarehouse Administrator',
+    discription: '"As a DBA I used to maintain IBM Netezza db machine, Informatica powercenter, sql server managment studio and Oracle sql developer"',
+    image: UWLogo
+  },
+     {
+    id:3,
+    title: 'Web Developer',
+    discription: '"I woulk like to switch grears and start working in a new stream of UI eveloper and my goal is to become a full stack developer"',
+    image: web_dev
+  },
+
+];
+
+export default PROJECTS;
---
+++ src/Data/socials.js
@@ -0,0 +1,24 @@
+import email from '../Social_assets/email_icon.png';
+import linkedin from '../Social_assets/linkedin_icon.png';
+import twitter from '../Social_assets/twitter_icon.png';
+
+const SOCIALS =[
+  {
+    id: 1,
+    snap: email,
+    link: 'https://mail.google.com/'
+  },
+  {
+    id: 2,
+    snap: linkedin,
+    link: 'https://www.linkedin.com/'
+  },
+  {
+    id: 3,
+    snap: twitter,
+    link: 'https://twitter.com/login'
+  }
+  
+]
+
+export default SOCIALS;
